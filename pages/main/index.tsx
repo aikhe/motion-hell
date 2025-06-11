a5252072b9e5fa51bb8d4ab5313b7af17d23f2f5
aikhe <ikeandrie.ro@gmail.com>
2025-06-12 03:32:54 +0800

    feat(pages): pages structure & initial for testing lenis scroll

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,9 +1,24 @@
 function MainPage() {
   return (
     <>
-      <p className="text-4xl">aikhe</p>
-      <p className="text-5xl">lulumi</p>
-      <p className="">maomi</p>
+      <div className="bg-black"></div>
+      {[...Array(4)].map((_, index) =>
+        index % 2 === 0 ? (
+          <div
+            key={index}
+            className="flex h-[100svh] w-full items-center justify-center bg-black"
+          >
+            <p className="text-2xl text-white">Lumi&lt;3</p>
+          </div>
+        ) : (
+          <div
+            key={index}
+            className="flex h-[100svh] w-full bg-black/20 items-center justify-center"
+          >
+            <p className="text-2xl">maomi&lt;3</p>
+          </div>
+        ),
+      )}
     </>
   );
 }
